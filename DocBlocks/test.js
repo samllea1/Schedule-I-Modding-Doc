a185ee26f7bd600e873137455666525f2ca4de4c
Chaosflo677 <florian.t.winter@gmail.com>
2025-11-24 12:04:24 +0100

    Delete DocBlocks directory

--- DocBlocks/test.js
+++
@@ -1,7 +0,0 @@
-
-                    // Function: test
-                    // Description: 123
-                    function test() {
-                        Debug.Log("Hello, world!");
-                    }
-                
